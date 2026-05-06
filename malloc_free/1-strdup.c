8dd1e75b426b7a08e310a3dbef2815506cecfa6e
wudutesfa <wudutesfap@gmail.com>
2026-05-06 05:57:25 -0700

    Final BETTY-compliant malloc_free solution

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,24 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strdup - returns a pointer to a duplicated string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL if failed
+ */
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int len = 0;
-    unsigned int i;
+	char *copy;
+	int i, len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    dup = malloc(sizeof(char) * (len + 1));
-    if (dup == NULL)
-        return (NULL);
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
 
-    for (i = 0; i <= len; i++)
-        dup[i] = str[i];
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
 
-    return (dup);
+	copy[len] = '\0';
+
+	return (copy);
 }
