4188f0b2e0edde206ed805c8901360a4a772a0b1
wudutesfa <wudutesfap@gmail.com>
2026-05-12 22:49:25 -0700

    All tasks completed and tested

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -10,7 +10,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -19,9 +18,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Prototypes go here with no documentation in the header */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-/* ... etc ... */
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
